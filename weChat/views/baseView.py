941265cc56251aab16e86cb18cc808840d3b4e39
zhenyuantg <936451076@qq.com>
2021-01-13 12:15:09 +0800

    add：新增刷新token的视图函数

--- weChat/views/baseView.py
+++ weChat/views/baseView.py
@@ -1,6 +1,9 @@
+import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from weChat.models import UserInfo
+from weChat.tools.makeRefreshToken import getRefreshToekn
 
 
 @api_view(['GET'])
@@ -13,7 +16,28 @@ def helloView(request):
     return Response(rsp)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def refreshTokenView(request):
     """通过Refresh Token 更改 AccessToken"""
+    refresh_token = request.data.get("refresh_token", "")
+    userObj = UserInfo.objects.filter(refresh_token=refresh_token).first()
+    # 假如改用户存在
+    if userObj.exists():
+        # 判断是否过期
+        if userObj.refresh_token_expire_date < datetime.datetime.now():
+            # 可以刷新
+            access_token = userObj.token(7)
+            refresh_token = getRefreshToekn(userObj.user_id)
+            expire_date = datetime.datetime.now() + datetime.timedelta(days=7)
+            userObj.refresh_token = refresh_token
+            userObj.refresh_token_expire_date = expire_date
+            userObj.save()
+            return Response({
+                "status": 200,
+                "access_token": access_token,
+                "expires_in": 604800,  # token有效期为7天
+                "refresh_token": refresh_token,
+                "nickName": userObj.nickname
+            })
+    return Response({"status": status.HTTP_204_NO_CONTENT, "mas": "请重新登录"})
 
