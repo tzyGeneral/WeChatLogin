24aae345af96a88638b3931d161f4e5d0dbc57e0
zhenyuantg <936451076@qq.com>
2021-01-13 12:16:14 +0800

    add：JWT检查时获取用户信息走缓存

--- weChat/tools/decorators.py
+++ weChat/tools/decorators.py
@@ -2,13 +2,13 @@ import jwt
 from functools import wraps
 from django.conf import settings
 from django.http import JsonResponse
-from weChat.models import UserInfo
+from weChat.cache.cachetool import UserInfoCache
 from weChat.config import SIGN_ERR, NOT_AUTHENTICATE
 
 
 def my_login_required(view_func):
     """自定义登陆验证，验证token是否过期"""
-    UserModel = UserInfo
+    UserModel = UserInfoCache()
 
     def _wrapped_view(request, *args, **kwargs):
         try:
@@ -21,7 +21,9 @@ def my_login_required(view_func):
                 try:
                     dic = jwt.decode(auth[1], settings.SECRET_KEY, algorithms=["HS256"])
                     user_id = dic.get("data").get("user_id")
-                    user = UserModel.objects.get(user_id=user_id)
+                    user = UserModel.getUserObj(user_id)
+                    if not user:
+                        raise ValueError("用户不存在")
                     request.user = user
                 except jwt.ExpiredSignatureError:
                     return JsonResponse({"status_code": SIGN_ERR, "message": "Token 过期"})
