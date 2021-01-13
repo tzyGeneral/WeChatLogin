943ad41c86fbfdff8afdc2775ac2484a3627ce9a
zhenyuantg <936451076@qq.com>
2021-01-13 12:16:47 +0800

    add：新增生成refreshToekn工具

---
+++ weChat/tools/makeRefreshToken.py
@@ -0,0 +1,8 @@
+from weChat.utils import getUTCtimesmap, getMd5EncodeData
+
+
+def getRefreshToekn(user_id: int) -> str:
+    """生成刷新token"""
+    string = str(user_id) + getUTCtimesmap()
+    return getMd5EncodeData(string)
+
