fdc3a9df490d28c03ab4f70a68e16cd5aacbec9a
zhenyuantg <936451076@qq.com>
2021-01-13 12:17:13 +0800

    add：新增utils方法

--- weChat/utils.py
+++ weChat/utils.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import hashlib
+import time
 
 
 def weChatRequest(url, encoding="urf-8"):
@@ -9,3 +11,21 @@ def weChatRequest(url, encoding="urf-8"):
         if response.status_code == 200: return response.json()
     except json.JSONDecodeError:
         return {}
+
+
+def getUTCtimesmap():
+    """
+    获取当前UTC时间的时间戳
+    :return:
+    """
+    return str(int(round(time.time() * 1000)))
+
+
+def getMd5EncodeData(string: str):
+    """
+    Md5加密字符串
+    :param string:
+    :return:
+    """
+    hash_md5 = hashlib.md5(string.encode('utf-8')).hexdigest()
+    return hash_md5
